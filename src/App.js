5ca516538371970a03d7f4c51570d43c1b57b9d9
PhanBaoThanh <thanhninjahero@gmail.com>
2022-10-11 16:44:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {useState} from 'react'
 
 function App() {
+  const [isOpen,setIsOpen] = useState(false)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='box'>
+        <h1 className='box__header'>Modal Project</h1>
+        <div className='box__btn'>
+          <div className='box__btn__item' onClick={() => setIsOpen(true)}>open modal</div>
+        </div>
+      </div>
+
+      <div className='modal' style={{display: `${isOpen ? 'flex' : ''}`}}>
+        <div className='modal__box'>
+          <div className='modal__box__closeBtn' onClick={() => setIsOpen(false)}>X</div>
+
+          <h2 className='modal__box__header'>Modal Content</h2>
+        </div>
+      </div>
     </div>
   );
 }
